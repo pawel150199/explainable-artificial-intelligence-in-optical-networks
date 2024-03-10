8bf3ce626b32c49929db0e31dd474dddc6577f8c
pawel150199 <pawel.polski99@gmail.com>
2024-03-10 22:44:53 +0100

    change logging

--- run_shapley_analysis.sh
+++ run_shapley_analysis.sh
@@ -18,7 +18,7 @@ check_status() {
 
 info "Run shapley analysis for Euro28 dataset..."
 info "Run shapley analysis for LinearRegression estimator."
-python3 shapley_analysis.py --dataset Euro28 --estimator LinearRegression --image_name euro28_linear_regression > /dev/null 2>&1
+python3 shapley_analysis.py --dataset Euro28 --estimator LinearRegression --image_name euro28_linear_regression 2>/dev/null
 check_status
 
 info "Run shapley analysis for SVR estimator."
@@ -26,35 +26,35 @@ python3 shapley_analysis.py --dataset Euro28 --estimator SVR --image_name euro28
 check_status
 
 info "Run shapley analysis for DecisionTreeRegressor estimator."
-python3 shapley_analysis.py --dataset Euro28 --estimator DecisionTreeRegressor --image_name euro28_cart > /dev/null 2>&1
+python3 shapley_analysis.py --dataset Euro28 --estimator DecisionTreeRegressor --image_name euro28_cart 2>/dev/null
 check_status
 
 info "Run shapley analysis for KNeighborsRegression estimator."
-python3 shapley_analysis.py --dataset Euro28 --estimator KNeighborsRegression --image_name euro28_knn > /dev/null 2>&1
+python3 shapley_analysis.py --dataset Euro28 --estimator KNeighborsRegression --image_name euro28_knn 2>/dev/null
 check_status
 
 info "Run shapley analysis for RandomForestRegressor estimator."
-python3 shapley_analysis.py --dataset Euro28 --estimator RandomForestRegressor --image_name euro28_knn > /dev/null 2>&1
+python3 shapley_analysis.py --dataset Euro28 --estimator RandomForestRegressor --image_name euro28_knn 2>/dev/null
 check_status
 
 
 info "Run shapley analysis for US28 dataset..."
 info "Run shapley analysis for LinearRegression estimator."
-python3 shapley_analysis.py --dataset US26 --estimator LinearRegression --image_name euro28_linear_regression > /dev/null 2>&1
+python3 shapley_analysis.py --dataset US26 --estimator LinearRegression --image_name euro28_linear_regression 2>/dev/null
 check_status
 
 info "Run shapley analysis for SVR estimator."
-python3 shapley_analysis.py --dataset US26 --estimator SVR --image_name euro28_svr > /dev/null 2>&1
+python3 shapley_analysis.py --dataset US26 --estimator SVR --image_name euro28_svr 2>/dev/null
 check_status
 
 info "Run shapley analysis for DecisionTreeRegressor estimator."
-python3 shapley_analysis.py --dataset US26 --estimator DecisionTreeRegressor --image_name euro28_cart > /dev/null 2>&1
+python3 shapley_analysis.py --dataset US26 --estimator DecisionTreeRegressor --image_name euro28_cart 2>/dev/null
 check_status
 
 info "Run shapley analysis for KNeighborsRegression estimator."
-python3 shapley_analysis.py --dataset US26 --estimator KNeighborsRegression --image_name euro28_knn > /dev/null 2>&1
+python3 shapley_analysis.py --dataset US26 --estimator KNeighborsRegression --image_name euro28_knn 2>/dev/null
 check_status
 
 info "Run shapley analysis for RandomForestRegressor estimator."
-python3 shapley_analysis.py --dataset US26 --estimator RandomForestRegressor --image_name euro28_knn > /dev/null 2>&1
+python3 shapley_analysis.py --dataset US26 --estimator RandomForestRegressor --image_name euro28_knn 2>/dev/null
 check_status
